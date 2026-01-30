647b6544a249c5ad1d918911b9591bd613cfe0f5
LillieLovesick <lillielovesick@gmail.com>
2026-01-30 03:08:45 +0000

    Added current target markers when in MMO Camera, Improved targeting logic (still needs touch ups), Lock-on target icon + animations for it, and finally some code optimisations
    I'll keep working on this targeting code until it kills me, or it FINALLY decides to stop bitching and whining and breaking every 5 seconds.

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -12,10 +12,15 @@ extends CharacterBody3D
 @export var rotation_speed := 12.0
 @export var jump_strength := 12.0
 
+@export_group("HUD")
+@export var preload_target_icon = PackedScene
+
 @onready var _camera_pivot: Node3D = %CamPivot
 @onready var _camera: Camera3D = %Camera3D
 @onready var _character: MeshInstance3D = %CharaModel
 @onready var _raycast: RayCast3D = %TargetChecker
+@onready var _target_symbol: Sprite3D = %TargetSymbol
+@onready var _target_symbol_animplayer: AnimationPlayer = %TargetAnimPlayer
 
 var _camera_input_direction := Vector2.ZERO
 var _last_movement_direction := Vector3.BACK
@@ -39,17 +44,24 @@ func _ready() -> void:
 func _targetCheck() -> void:
 	for _entity in _target_list:
 		if _entity is CharacterBody3D or _entity is RigidBody3D:
+			_target_list = $CamPivot/TargetArea.get_overlapping_bodies()
 			_raycast.set_target_position(_entity.global_position)
 			_raycast.force_raycast_update()
 			if _raycast.get_collider() is not PhysicsBody3D and _raycast.get_collider() != null:
 				_target_list.erase(_entity)
-				print("erased")
-				print(_target_list)
 			else:
 				var distance = _entity.global_position.distance_squared_to(self.global_position)
 				if distance < _closest_target:
+					if _target == null:
+						remove_child(_target_symbol)
+					else:
+						_target.remove_child(_target_symbol)	
 					_target = _entity
-					locked_on = true
+					_target.add_child(_target_symbol)
+
+					var _target_location = _target.get_node("TargetLocation")
+					_target_symbol.position = _target_location.position
+					_target_symbol.visible = true
 					_closest_target = distance
 
 
@@ -65,16 +77,21 @@ func _input(event: InputEvent) -> void:
 		camera_snapped = false
 		locked_on = false
 		_target_list = $CamPivot/TargetArea.get_overlapping_bodies()
-		print("INITIAL TARGET")
-		print(_target_list)
 		_targetCheck()
-		print(locked_on)
+		_target = _target_symbol.get_parent()
+		locked_on = true
+		_target_symbol_animplayer.play("Target_Lockon")
 		
 	if event.is_action_released("right_click"):
 		camera_mode = 0
 		camera_snapped = false
 		locked_on = false
 		_closest_target = INF
+		if _target != null:
+			_target.remove_child(_target_symbol)
+			add_child(_target_symbol)
+			_target_symbol.visible = false
+			_target_symbol_animplayer.play("RESET")
 		_target = null
 		_raycast.set_target_position(Vector3(0,0,60))
 		
@@ -83,10 +100,9 @@ func _input(event: InputEvent) -> void:
 		_closest_target = INF
 		_target = null
 		_target_list = $CamPivot/TargetArea.get_overlapping_bodies()
-		print("RETARGET")
-		print(_target_list)
 		_targetCheck()
-		print(locked_on)
+		_target = _target_symbol.get_parent()
+		locked_on = true
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
@@ -100,9 +116,13 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func _process(delta: float) -> void:
 	if camera_mode == 0:
+		_targetCheck()
+		
+	if camera_mode == 0:
 		_camera_pivot.rotation.x -= _camera_input_direction.y * delta
 		_camera_pivot.rotation.x = clamp(_camera_pivot.rotation.x,-PI / 6.0, PI / 3.0)
 		_camera_pivot.rotation.y -= _camera_input_direction.x * delta
+		
 	if camera_mode == 1:
 		if locked_on == true:
 			_camera_pivot.look_at(_target.global_position, Vector3.UP)
@@ -117,6 +137,7 @@ func _process(delta: float) -> void:
 	_camera_input_direction = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
+		
 	if camera_mode == 0 and camera_snapped == false:
 		tween = get_tree().create_tween()
 		tween.set_parallel()
@@ -161,3 +182,13 @@ func _physics_process(delta: float) -> void:
 		velocity.y += jump_strength
 	
 	move_and_slide()
+
+
+func _on_target_area_body_entered(body: Node3D) -> void:
+	if body is CharacterBody3D or body is RigidBody3D:
+		_target_list = $CamPivot/TargetArea.get_overlapping_bodies()
+
+
+func _on_target_area_body_exited(body: Node3D) -> void:
+	if body is CharacterBody3D or body is RigidBody3D:
+		_target_list = $CamPivot/TargetArea.get_overlapping_bodies()
