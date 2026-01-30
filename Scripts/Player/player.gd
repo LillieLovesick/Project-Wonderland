fd5432bd1a75ed5e954398234497a2a737d2cee0
LillieLovesick <lillielovesick@gmail.com>
2026-01-30 05:24:51 +0000

    Higher Quality target selector graphics + A *LOT* of targeting bugfixes.
    Retargeting is DEAD for now until I either figure out a better way to do it or scrap the code entirely.

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -19,6 +19,7 @@ extends CharacterBody3D
 @onready var _camera: Camera3D = %Camera3D
 @onready var _character: MeshInstance3D = %CharaModel
 @onready var _raycast: RayCast3D = %TargetChecker
+@onready var _target_area: Area3D = %TargetArea
 @onready var _target_symbol: Sprite3D = %TargetSymbol
 @onready var _target_symbol_animplayer: AnimationPlayer = %TargetAnimPlayer
 
@@ -44,7 +45,7 @@ func _ready() -> void:
 func _targetCheck() -> void:
 	for _entity in _target_list:
 		if _entity is CharacterBody3D or _entity is RigidBody3D:
-			_target_list = $CamPivot/TargetArea.get_overlapping_bodies()
+			_target_list = _target_area.get_overlapping_bodies()
 			_raycast.set_target_position(_entity.global_position)
 			_raycast.force_raycast_update()
 			if _raycast.get_collider() is not PhysicsBody3D and _raycast.get_collider() != null:
@@ -54,14 +55,18 @@ func _targetCheck() -> void:
 				if distance < _closest_target:
 					if _target == null:
 						remove_child(_target_symbol)
-					else:
-						_target.remove_child(_target_symbol)	
+					elif _target != null:
+						_target.remove_child(_target_symbol)
+						
 					_target = _entity
 					_target.add_child(_target_symbol)
-
-					var _target_location = _target.get_node("TargetLocation")
-					_target_symbol.position = _target_location.position
 					_target_symbol.visible = true
+
+					var target_check = _target_symbol.get_parent().name
+					if target_check != "Player":
+						var _target_location = _target.get_node("TargetLocation")
+						_target_symbol.position = _target_location.position
+						_target_symbol.scale = _target_location.scale
 					_closest_target = distance
 
 
@@ -76,11 +81,14 @@ func _input(event: InputEvent) -> void:
 		camera_mode = 1
 		camera_snapped = false
 		locked_on = false
-		_target_list = $CamPivot/TargetArea.get_overlapping_bodies()
+		_closest_target = INF
+		_target_list = _target_area.get_overlapping_bodies()
 		_targetCheck()
-		_target = _target_symbol.get_parent()
-		locked_on = true
-		_target_symbol_animplayer.play("Target_Lockon")
+		var target_check = _target_symbol.get_parent().name
+		if target_check != "Player":
+			_target = _target_symbol.get_parent()
+			locked_on = true
+			_target_symbol_animplayer.play("Target_Lockon")
 		
 	if event.is_action_released("right_click"):
 		camera_mode = 0
@@ -93,16 +101,23 @@ func _input(event: InputEvent) -> void:
 			_target_symbol.visible = false
 			_target_symbol_animplayer.play("RESET")
 		_target = null
-		_raycast.set_target_position(Vector3(0,0,60))
+		_raycast.set_target_position(Vector3(0,0,62))
 		
-	if event.is_action_pressed("retarget") and camera_mode == 1:
-		locked_on = false
-		_closest_target = INF
-		_target = null
-		_target_list = $CamPivot/TargetArea.get_overlapping_bodies()
-		_targetCheck()
-		_target = _target_symbol.get_parent()
-		locked_on = true
+#	if event.is_action_pressed("retarget") and camera_mode == 1:
+#		_closest_target = INF
+#		_target = null
+#		
+#		_target_list = _target_area.get_overlapping_bodies()
+#		_targetCheck()
+#		var target_check = _target_symbol.get_parent().name
+#		if target_check != "Player":
+#			_target = _target_symbol.get_parent()
+#			_target_symbol_animplayer.play("Target_Lockon")
+#		locked_on = true
+		
+	if event.is_action_pressed("debug_toggle"):
+		print(_target)
+		print(_target_symbol.get_parent())
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
@@ -115,9 +130,6 @@ func _unhandled_input(event: InputEvent) -> void:
 				_camera_input_direction = event.screen_relative * mouse_sensitivity
 
 func _process(delta: float) -> void:
-	if camera_mode == 0:
-		_targetCheck()
-		
 	if camera_mode == 0:
 		_camera_pivot.rotation.x -= _camera_input_direction.y * delta
 		_camera_pivot.rotation.x = clamp(_camera_pivot.rotation.x,-PI / 6.0, PI / 3.0)
@@ -186,9 +198,21 @@ func _physics_process(delta: float) -> void:
 
 func _on_target_area_body_entered(body: Node3D) -> void:
 	if body is CharacterBody3D or body is RigidBody3D:
-		_target_list = $CamPivot/TargetArea.get_overlapping_bodies()
+		_target_list = _target_area.get_overlapping_bodies()
+		_targetCheck()
 
 
 func _on_target_area_body_exited(body: Node3D) -> void:
 	if body is CharacterBody3D or body is RigidBody3D:
-		_target_list = $CamPivot/TargetArea.get_overlapping_bodies()
+		if body == _target:
+			_target.remove_child(_target_symbol)
+			_target_symbol.visible = false
+			add_child(_target_symbol)
+			_target_symbol_animplayer.play("RESET")
+			
+			_target = null
+			locked_on = false
+		_closest_target = INF
+		
+		_target_list = _target_area.get_overlapping_bodies()
+		_targetCheck()
