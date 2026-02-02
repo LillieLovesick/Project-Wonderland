eba1c5068bd14649ca454be9c1a6c2ac02862911
LillieLovesick <lillielovesick@gmail.com>
2026-02-02 00:05:12 +0000

    Update to Godot 4.6 + Player Script rewrite STAGE 1
    Basic movement + Optimizations done. Need to implement OTS camera, targeting, Multi-target, Target switching (maybe), and more.

--- Scripts/Player/playerNEW.gd
+++ Scripts/Player/playerNEW.gd
@@ -6,22 +6,18 @@ extends CharacterBody3D
 @export var zoom_speed := 0.1
 
 @export_group("Movement")
-@export var move_speed := 8.0
-@export var acceleration := 10.0
-@export var deceleration := 75.0
+@export var move_speed := 16.0
+@export var acceleration := 50.0
 @export var rotation_speed := 12.0
 @export var jump_strength := 12.0
+@export var stopping_speed := 8.0
 
 @export_group("HUD")
-@export var preload_target_icon = PackedScene
+@export var targetIcon = Texture
 
 @onready var _camera_pivot: Node3D = %CamPivot
 @onready var _camera: Camera3D = %Camera3D
 @onready var _character: MeshInstance3D = %CharaModel
-@onready var _raycast: RayCast3D = %TargetChecker
-@onready var _target_area: Area3D = %TargetArea
-@onready var _target_symbol: Sprite3D = %TargetSymbol
-@onready var _target_symbol_animplayer: AnimationPlayer = %TargetAnimPlayer
 
 var _camera_input_direction := Vector2.ZERO
 var _last_movement_direction := Vector3.BACK
@@ -31,8 +27,6 @@ var tween
 var camera_snapped
 var locked_on
 var _target
-var _closest_target = INF
-var _target_list: Array[Node3D]
 
 func _ready() -> void:
 	if camera_mode == 0:
@@ -42,34 +36,6 @@ func _ready() -> void:
 		$CamPivot/SpringArm3D.spring_length = 2.0
 		$CamPivot/SpringArm3D.position.x = -1.2
 
-func _targetCheck() -> void:
-	for _entity in _target_list:
-		if _entity is CharacterBody3D or _entity is RigidBody3D:
-			_target_list = _target_area.get_overlapping_bodies()
-			_raycast.set_target_position(_entity.global_position)
-			_raycast.force_raycast_update()
-			if _raycast.get_collider() is not PhysicsBody3D and _raycast.get_collider() != null:
-				_target_list.erase(_entity)
-			else:
-				var distance = _entity.global_position.distance_squared_to(self.global_position)
-				if distance < _closest_target:
-					if _target == null:
-						remove_child(_target_symbol)
-					elif _target != null:
-						_target.remove_child(_target_symbol)
-						
-					_target = _entity
-					_target.add_child(_target_symbol)
-					_target_symbol.visible = true
-
-					var target_check = _target_symbol.get_parent().name
-					if target_check != "Player":
-						var _target_location = _target.get_node("TargetLocation")
-						_target_symbol.position = _target_location.position
-						_target_symbol.scale = _target_location.scale
-					_closest_target = distance
-
-
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("left_click"):
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -78,56 +44,21 @@ func _input(event: InputEvent) -> void:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		
 	if event.is_action_pressed("right_click"):
-		camera_mode = 1
-		camera_snapped = false
-		locked_on = false
-		_closest_target = INF
-		_target_list = _target_area.get_overlapping_bodies()
-		_targetCheck()
-		var target_check = _target_symbol.get_parent().name
-		if target_check != "Player":
-			_target = _target_symbol.get_parent()
-			locked_on = true
-			_target_symbol_animplayer.play("Target_Lockon")
+		pass
 		
 	if event.is_action_released("right_click"):
-		camera_mode = 0
-		camera_snapped = false
-		locked_on = false
-		_closest_target = INF
-		if _target != null:
-			_target.remove_child(_target_symbol)
-			add_child(_target_symbol)
-			_target_symbol.visible = false
-			_target_symbol_animplayer.play("RESET")
-		_target = null
-		_raycast.set_target_position(Vector3(0,0,62))
-		
-#	if event.is_action_pressed("retarget") and camera_mode == 1:
-#		_closest_target = INF
-#		_target = null
-#		
-#		_target_list = _target_area.get_overlapping_bodies()
-#		_targetCheck()
-#		var target_check = _target_symbol.get_parent().name
-#		if target_check != "Player":
-#			_target = _target_symbol.get_parent()
-#			_target_symbol_animplayer.play("Target_Lockon")
-#		locked_on = true
+		pass
 		
 	if event.is_action_pressed("debug_toggle"):
-		print(_target)
-		print(_target_symbol.get_parent())
+		if Globals.Debug_mode == false:
+			Globals.Debug_mode = true
+		else:
+			Globals.Debug_mode = false
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if camera_mode == 0:
 			_camera_input_direction = event.screen_relative * mouse_sensitivity
-		elif camera_mode == 1:
-			if locked_on == true:
-				pass
-			else:
-				_camera_input_direction = event.screen_relative * mouse_sensitivity
 
 func _process(delta: float) -> void:
 	if camera_mode == 0:
@@ -148,7 +79,9 @@ func _process(delta: float) -> void:
 		
 	_camera_input_direction = Vector2.ZERO
 
+
 func _physics_process(delta: float) -> void:
+
 	
 	if camera_mode == 0 and camera_snapped == false:
 		tween = get_tree().create_tween()
@@ -174,6 +107,7 @@ func _physics_process(delta: float) -> void:
 	
 	if move_direction.length() > 0.2:
 		_last_movement_direction = move_direction
+		
 	#MMO Camera Character Rotation
 	if camera_mode == 0:
 		var target_angle := Vector3.BACK.signed_angle_to(_last_movement_direction, Vector3.UP)
@@ -183,36 +117,14 @@ func _physics_process(delta: float) -> void:
 	var y_velocity := velocity.y
 	
 	velocity.y = 0.0
-	if raw_input:
-		velocity = velocity.move_toward(move_direction * move_speed, acceleration * delta)
-	else:
-		velocity = velocity.move_toward(move_direction * move_speed, deceleration * delta)
+	velocity = velocity.move_toward(move_direction * move_speed, acceleration * delta)
 	velocity.y = y_velocity + _gravity * delta
+	
+	if is_equal_approx(move_direction.length(), 0.0) and velocity.length() < stopping_speed:
+		velocity = Vector3(0,velocity.y,0)
 	
 	var is_starting_jump := Input.is_action_just_pressed("jump") and is_on_floor()
 	if is_starting_jump:
 		velocity.y += jump_strength
 	
 	move_and_slide()
-
-
-func _on_target_area_body_entered(body: Node3D) -> void:
-	if body is CharacterBody3D or body is RigidBody3D:
-		_target_list = _target_area.get_overlapping_bodies()
-		_targetCheck()
-
-
-func _on_target_area_body_exited(body: Node3D) -> void:
-	if body is CharacterBody3D or body is RigidBody3D:
-		if body == _target:
-			_target.remove_child(_target_symbol)
-			_target_symbol.visible = false
-			add_child(_target_symbol)
-			_target_symbol_animplayer.play("RESET")
-			
-			_target = null
-			locked_on = false
-		_closest_target = INF
-		
-		_target_list = _target_area.get_overlapping_bodies()
-		_targetCheck()
